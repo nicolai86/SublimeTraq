06e270be0276874b127a51c035f80f5ec711e238
Raphael Randschau <nicolai86@me.com>
2013-11-01 21:35:20 +0100

    traq integration works

--- sublime_traq.py
+++ sublime_traq.py
@@ -6,7 +6,12 @@ import subprocess
 
 class SublimeTraqCommand(sublime_plugin.WindowCommand):
   def run(self):
-    command = "{}/bin/traq -p example -m 10".format(os.environ["TRAQ_PATH"])
+    view = self.window.active_view()
+    self.window.show_input_panel("traq:", "", lambda user_input: self.traq(view, user_input), None, None)
+
+  def traq(self, view, command):
+    # TODO validate available commands
+    command = "{}/bin/traq {}".format(os.environ["TRAQ_PATH"], command)
     cmd = [command]
 
     proc_env = os.environ.copy()
@@ -14,10 +19,13 @@ class SublimeTraqCommand(sublime_plugin.WindowCommand):
       proc_env[k] = os.path.expandvars(v).encode(sys.getfilesystemencoding())
 
     process = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True, env=proc_env)
-    output = process.stdout.read()
-    pprint.pprint(output)
+    output = str(process.stdout.read(), "utf-8")
 
-    sublime.message_dialog(str(output, "utf-8"))
+    if len(output) > 0:
+      view = self.window.new_file()
+      view.run_command('append', {
+        'characters': output,
+      })
 
   def is_enabled(self):
     return True
